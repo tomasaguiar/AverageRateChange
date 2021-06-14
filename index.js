fc990e6fe3f7b80ddc53bb58db503e77fce3dfcc
Tomás Aguiar <aguiarhcode@gmail.com>
2021-06-14 15:11:16 +0100

    Conditions to support various types of functions, bug fixes

--- index.js
+++ index.js
@@ -3,28 +3,57 @@
 const userInput = require("prompt-sync")
 const prompt = userInput()
 
-let func = prompt("f(x)= ").replace('\^', '**')
+app()
 
-let x = prompt("Digite o primeiro valor do intervalo: ") 
+function app() {
 
-var valueHandler = x
+    let func = prompt("f(x)= ").replace('\^', '**')
 
-var result = eval(func)
+    input()
 
-if (result == result) {
+    function input() {
 
-    let x = prompt("Digite o segundo valor do intervalo: ")
-    var resultHandler = eval(func)
-    var averageChangeRate = (resultHandler - result) / (x - valueHandler)
+    let x = prompt("Digite o extremo inferior do intervalo: ") 
+    
+    let fA = x
+    
+        if (fA == Infinity, NaN) {
+            console.log("O extremo " + x + " não pertence ao domínio da função.")
+            input()
+            return;
+        }
+    
+    var result = eval(func) 
+    
+    if (result == result) {
+    
+        let fB = prompt("Digite o extremo superior do intervalo: ")
+        x = fB
+        var resultHandler = eval(func)
+        var averageChangeRate = (resultHandler - result) / (x - fA)
+    }
+    
+    if (x == Infinity, NaN) {
+        console.log("O extremo " + x + " não pertence ao domínio da função.")
+        input()
+        return;   
+    }
+    
+    if (fA > x) {
+        console.log("O extremo inferior não pode ser maior que o extremo superior.")
+        input()
+        return;
+    }
+    
+    console.log("A taxa média de variação da função: " + func + " no intervalo: " +  "[" + fA + " , " + x + "]" + " é: " + averageChangeRate)
+    
+    // Debugging 
+    // console.log("Ferramentas de debugging")
+    // console.log(func)
+    // console.log(x)
+    // console.log(fA)
+    // console.log(result)
+    // console.log(resultHandler)
+    // console.log(averageChangeRate)
+    }
 }
-
-console.log("A taxa média de variação da função: " + func + " é: " + averageChangeRate)
-
-// Debugging 
-
-console.log("Ferramentas de debugging")
-console.log(func)
-console.log(x)
-console.log(valueHandler)
-console.log(result)
-console.log(resultHandler)
